f4dcec7b5e7420bc0b3791b0599f58582acebfe8
goutham-o <studygouthamm@gmail.com>
2021-03-14 01:20:13 +0530

    Initial Author Routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        errorMessage: ''
+    });
 })
 
 module.exports = router
